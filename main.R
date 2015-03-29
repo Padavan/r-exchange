8c888a1095f73e6838ecf6d7145880237f7bc7ef
Dmitry Shihaleev <padavankun@gmail.com>
2015-03-30 01:24:44 +0300

    copypaste old code

--- main.R
+++ main.R
@@ -1,12 +1,13 @@
 print("helloworld")
 
-require(ggplot2)
-#library(ggplot2)
+#require(ggplot2)
+library(ggplot2)
 
 dat <- read.csv('dataset1.csv', sep = '\t')
 
+g <- ggplot(dat, aes(x = A, y = U)) +
+  geom_point()+
+  geom_line() +
+  scale_x_log10(breaks = c(5,10,15,20,25,30,35))
 
-for (i in 1:5)
-{
-  print("oh dear, I am programming in R!!")
-}
+plot(g)
