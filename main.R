300d6b2f4bf7d320bc221ddaa5bd9d195f68fa57
Sergej Hartfil <sergej.hartfil@googlemail.com>
2015-03-30 00:27:38 +0200

    make it black!

--- main.R
+++ main.R
@@ -8,6 +8,7 @@ dat <- read.csv('dataset1.csv', sep = '\t')
 g <- ggplot(dat, aes(x = A, y = U)) +
   geom_point()+
   geom_line() +
-  scale_x_log10(breaks = c(5,10,15,20,25,30,35))
+  scale_x_log10(breaks = c(5,10,15,20,25,30,35)) +
+  theme_bw()
 
 plot(g)
