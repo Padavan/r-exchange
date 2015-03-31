33e97430ecad78506822bd083ee8b2cb316f4a71
Dmitry Shihaleev <padavankun@gmail.com>
2015-03-31 16:45:00 +0300

    table generation stuff

--- main.R
+++ main.R
@@ -1,14 +1,16 @@
 print("helloworld")
 
-#require(ggplot2)
 library(ggplot2)
 
 dat <- read.csv('dataset1.csv', sep = '\t')
-
-g <- ggplot(dat, aes(x = A, y = U)) +
+datFixed <- dat
+datFixed$U <- dat$U - .003
+g <- ggplot(datFixed, aes(x = A, y = U)) +
   geom_point()+
   geom_line() +
-  scale_x_log10(breaks = c(5,10,15,20,25,30,35)) +
-  theme_bw()
-
+  scale_x_log10(breaks = c(5,10,15,20,25,30,35))
+  #theme_bw()
 plot(g)
+ggsave(file="laser-curve.png")
+
+
